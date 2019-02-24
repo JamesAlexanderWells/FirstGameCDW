0a360aee7011a0a48d1a80f16b95c41648bb9add
freddie coles <freddie.coles1@hotmail.co.uk>
2019-02-24 02:48:57 +0000

    Boss'

--- Assets/Scripts/BossScripts/Flower.cs
+++ Assets/Scripts/BossScripts/Flower.cs
@@ -1,29 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Assets.Scripts.Rooms;
 
 public class Flower : MonoBehaviour
 {
+    public GameObject tentacle;
     public Transform target;
     private float range;
     public float speed;
     private Animator anim;
     private float currentY;
     private float previousY;
+    private float tentacleHTimer;
+    private float tentacleWTimer;
+    private bool tentacleHSpawn;
+    private bool tentacleWSpawn;
+    private float tentacleH;
+    private float tentacleW;
+    private float rH;
+    private float rW;
+    private float Height;
+    private float Width;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         currentY = this.transform.position.y;
         previousY = 0;
+        tentacleHTimer = Time.time;
+        tentacleWTimer = Time.time;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Debug.Log(currentY);
-        Debug.Log(previousY);
+
         Vector3 theScale = transform.localScale;
         range = Vector2.Distance(transform.position, target.position);
         if (this.transform.position.x > target.transform.position.x)
@@ -42,6 +57,26 @@ public class Flower : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, target.position, -1*speed * Time.deltaTime);
         }
         GoingUp();
+        if (tentacleHSpawn)
+        {
+            tentacleHSpawn = false;
+            rH = GetRandomNumber(0, Height);
+
+        }
+        if (tentacleWSpawn)
+        {
+            tentacleWSpawn = false;
+        }
+        if (Time.time - tentacleHTimer > 1)
+        {
+            tentacleHTimer = Time.time;
+            tentacleHSpawn = true;
+        }
+        if (Time.time - tentacleWTimer > 2)
+        {
+            tentacleWTimer = Time.time;
+            tentacleWSpawn = true;
+        }
     }
     bool GoingUp()
     {
@@ -57,5 +92,10 @@ public class Flower : MonoBehaviour
             anim.SetBool("isBackwards", false);
             return false;           
         }
+    }
+    public float GetRandomNumber(float minimum, float maximum)
+    {
+        System.Random random = new System.Random();
+        return random.Next() * (maximum - minimum) + minimum;
     }
 }
---
+++ Assets/Scripts/BossScripts/Mantis.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mantis : MonoBehaviour
+{
+    public Transform target;
+    private SpriteRenderer sprite;
+    private float range;
+    public float speed;
+    private Animator anim;
+    private float rangex;
+    private float rangey;
+    public float shootTimer;
+    private float shootcountdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        shootcountdown = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("attack", false);
+        anim.SetBool("attackdown", false);
+        Vector3 theScale = transform.localScale;
+        range = Vector2.Distance(transform.position, target.position);
+        rangex = transform.position.x - target.position.x;
+        rangey = transform.position.y - target.position.y;
+        if (this.transform.position.x > target.transform.position.x)
+        {
+            theScale.x = 2;
+            transform.localScale = theScale;
+        }
+        else
+        {
+            theScale.x = -2;
+            transform.localScale = theScale;
+        }
+        if (rangey < 2)
+        {
+            sprite.sortingOrder = 2;
+        }
+        else
+        {
+            sprite.sortingOrder = 0;
+        }
+        if (rangex > 3 || rangey > 4 || -rangex > 3 || -rangey > 4)
+        {
+            if (!this.anim.GetCurrentAnimatorStateInfo(0).IsName("shoot"))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
+        if (range <= 5f)
+            if (rangex > rangey - 2 || -rangex > rangey - 2)
+            {
+
+                anim.SetBool("attack", true);
+            }
+            else if(rangey <= 5)
+            {
+                anim.SetBool("attackdown", true);
+            }
+        if (Time.time - shootcountdown > shootTimer)
+        {
+            anim.SetBool("shoot", true);
+            shootcountdown = Time.time;
+        }
+        else
+        {
+            anim.SetBool("shoot", false);
+        }
+        
+
+    }
+}
