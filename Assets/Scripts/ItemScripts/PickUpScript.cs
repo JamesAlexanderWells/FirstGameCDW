dfd373bfa84c772003c8f68b962cb5226df8d6ba
James Alexander Wells <cw001996@live.reading.ac.uk>
2019-02-24 02:07:30 +0000

    combining player movement with room

---
+++ Assets/Scripts/ItemScripts/PickUpScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public int healthIncrease;
+    public int damageIncrease;
+    public int speedIncrease;
+    public GameObject otherPowerUp1;
+    public GameObject otherPowerUp2;
+    public GameObject playerObject;
+    public int presidence;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D thing)
+    {
+        if (thing.name.Contains("player"))
+        {
+
+            applyPowerups();
+            destroyObjects();
+        }
+
+    }
+
+
+    private void applyPowerups()
+    {
+        playerObject.GetComponent<PlayerScript>().maxHealth += healthIncrease;
+        playerObject.GetComponent<PlayerScript>().damage += damageIncrease;
+        playerObject.GetComponent<PlayerScript>().speed += speedIncrease;
+        playerObject.GetComponent<PlayerScript>().pickUpList.Add(this);
+    }
+
+    private void destroyObjects()
+    {
+        Destroy(otherPowerUp1);
+        Destroy(otherPowerUp2);
+        Destroy(this.gameObject);
+    }
+}
