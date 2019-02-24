1c46d57c76b3569712fe03d7fd56017833f356c2
James <jrmd202@exeter.ac.uk>
2019-02-24 00:02:41 +0000

    Load prefabs from resources to avoid prefab misery

--- Assets/Scripts/Rooms/RoomController.cs
+++ Assets/Scripts/Rooms/RoomController.cs
@@ -9,12 +9,20 @@ namespace Assets.Scripts.Rooms
         // Use this for initialization
         void Start()
         {
-            CurrentRoom = Instantiate(CurrentRoom);
+            // Lets find another way some day.
+            var getBaseRoom = Resources.Load("BaseRoom") as GameObject;
+            var getDirt = Resources.Load("Dirt") as GameObject;
+            var getWall = Resources.Load("Wall") as GameObject;
+
+            CurrentRoom = Instantiate(getBaseRoom);
             var baseRoom = CurrentRoom.GetComponent<BaseRoom>();
 
             baseRoom.Player = GameObject.Find("dummyPlayer");
             baseRoom.Width = Random.Range(10, 20);
             baseRoom.Height = Random.Range(10, 20);
+
+            baseRoom.FloorTiles.Add(getDirt);
+            baseRoom.WallTiles.Add(getWall);
         }
 
         // Update is called once per frame
