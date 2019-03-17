db07bda75d7003b958ad77afefae496e0cf7d8db
Freddie <freddie.coles1@hotmail.co.uk>
2019-03-17 14:52:23 +0000

    player/boss interactions

---
+++ Assets/Scripts/BossScripts/BossScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    public float Health;
+    public int Damage;
+    public float Speed;
+    public float Aggresion;
+    public float Size;
+    public GameObject Target;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Attack();
+        Death();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        TakeDamage(col);
+        DealDamage(col);
+    }
+
+    
+    public virtual void Movement()
+    {
+
+    }
+    
+    public void Attack()
+    {
+
+    }
+
+    public void TakeDamage(Collider2D col)
+    {
+        if (col.name.Contains("Bullet") && col.name.Contains("Clone"))
+        {
+            Health -= player.GetComponent<PlayerScript>().damage;
+        }
+    }
+    public void DealDamage(Collider2D col)
+    {
+        if (col.name.Contains("Player"))
+        {
+            player.GetComponent<PlayerScript>().currentHealth -= Damage;
+        }
+    }
+    public void Death()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+    public void KnockBack()
+    {
+
+    }
+}
---
+++ Assets/Scripts/BossScripts/Fly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : BossScript
+{
+    private float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Target = GameObject.Find("dummyPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Death();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        TakeDamage(col);
+        DealDamage(col);
+    }
+
+    public override void Movement()
+    {
+        range = Vector2.Distance(transform.position, Target.transform.position);
+
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, Speed * Time.deltaTime);
+    }
+}
